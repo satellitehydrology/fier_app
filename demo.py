5f0fac09923482bb1787f9781cef47e21343237e
Son Do <sondo@utexas.edu>
2022-04-19 00:36:20 -0500

    apps

--- demo.py
+++ demo.py
@@ -5,8 +5,11 @@ st.set_page_config(layout="wide")
 
 
 # Title and Description
-st.title("Forecasting Inundation Extents using REOF Analysis (FIER) Over South-Southeast Asia")
+st.title("Forecasting Inundation Extents using REOF Analysis (FIER) Demo Apps")
 
-st.header('FIER Demo App Over Bangladesh:')
-st.write("[link](https://share.streamlit.io/skd862/fier_bangladesh/main/demo.py)")
+st.header("[FIER Demo App Over Bangladesh](https://share.streamlit.io/skd862/fier_bangladesh/main/demo.py)")
+
+st.header("[FIER Demo App Over Mekong Area](https://share.streamlit.io/skd862/fier_mekong/main/demo.py)")
+
+st.header("[FIER Demo App For VIIRS/ABI Water Fraction](https://share.streamlit.io/skd862/fier_noaa/main/demo.py)")
 
