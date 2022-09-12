b3e2a779d16195f9e0d0407fefefab45f913decd
Son Do <63930958+skd862@users.noreply.github.com>
2022-09-12 14:16:12 -0500

    testing

--- demo.py
+++ demo.py
@@ -15,4 +15,6 @@ st.header("[FIER Demo App For VIIRS/ABI Water Fraction](https://share.streamlit.
 
 url = "https://www.sciencedirect.com/science/article/pii/S0034425720301024?casa_token=kOYlVMMWkBUAAAAA:fiFM4l6BUzJ8xTCksYUe7X4CcojddbO8ybzOSMe36f2cFWEXDa_aFHaGeEFlN8SuPGnDy7Ir8w"
 st.write("Reference: [Chang, C. H., Lee, H., Kim, D., Hwang, E., Hossain, F., Chishtie, F., ... & Basnayake, S. (2020). Hindcast and forecast of daily inundation extents using satellite SAR and altimetry data with rotated empirical orthogonal function analysis: Case study in Tonle Sap Lake Floodplain. Remote Sensing of Environment, 241, 111732.](%s)" % url)
-    
+
+
+st.write('Testing')
